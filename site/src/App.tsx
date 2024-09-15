d042ed940c6699aab5e6fba9bce9a7b6408da16f
Hristo Roque <aroquega@inka-labs.com>
2024-09-15 09:52:45 -0500

    chore: Finish desktop bugs and table-desktop in-between

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -17,13 +17,13 @@ function App() {
   return (
     <>
       <header className="flex md:items-stretch">
-        <div className="py-10 px-6 lg:pl-[165px] flex items-center justify-between flex-1">
+        <div className="py-10 px-6 xl:pl-[165px] flex items-center justify-between flex-1">
           <img src={logo} />
           <div className="md:hidden">
             <img src={menu} />
           </div>
         </div>
-        <nav className="hidden md:block bg-red h-full py-[57px] pl-[33px] pr-10 lg:pl-[69px] lg:pr-[165px]">
+        <nav className="hidden md:block bg-red h-full py-[57px] pl-[33px] pr-10 xl:pl-[69px] lg:pr-[165px]">
           <ul className="flex h-full space-x-5 text-white items-center">
             <li>About</li>
             <li>Service</li>
@@ -37,11 +37,11 @@ function App() {
         </nav>
       </header>
       <img className="sm:hidden w-full" src={heroMobile} alt="Our Team" />
-      <div className="px-6 md:px-[39px] md:pb-[150px] lg:pt-[112px] lg:pl-[165px] pt-14 md:pt-[77px] pb-24 sm:bg-hero-tablet lg:bg-hero-desktop bg-no-repeat bg-right">
-        <h1 className="sm:max-w-[398px] lg:max-w-[550px] text-[40px] md:text-[56px] lg:text-[80px] font-extrabold leading-none">
+      <div className="px-6 md:px-[39px] md:pb-[150px] xl:pt-[112px] lg:pl-[165px] pt-14 md:pt-[77px] pb-24 sm:bg-hero-tablet lg:bg-hero-desktop bg-no-repeat bg-right">
+        <h1 className="sm:max-w-[398px] xl:max-w-[550px] text-[40px] md:text-[56px] lg:text-[80px] font-extrabold leading-none">
           Branding & website design agency
         </h1>
-        <p className="sm:max-w-[398px] lg:max-w-[540px] text-[15px] lg:text-[18px] mt-[15px] md:mt-[24px] mb-10 md:mb-8 font-normal leading-normal h-[75px]">
+        <p className="sm:max-w-[398px] xl:max-w-[540px] text-[15px] lg:text-[18px] mt-[15px] md:mt-[24px] mb-10 md:mb-8 font-normal leading-normal h-[75px]">
           We specialize in visual storytelling by creating cohesive brand and
           website design solutions for small businesses, giving lasting
           impressions to audiences in a digital world.
@@ -52,28 +52,28 @@ function App() {
       </div>
       <div className="sm:flex">
         <img src={imageStrategic} alt="" className="sm:hidden w-full" />
-        <img src={imageStrategicTablet} className="hidden sm:block lg:hidden" />
-        <img src={imageStrategicDesktop} className="hidden lg:block" />
-        <div className="px-6 bg-black text-white py-[72px] sm:pt-[120px] md:pl-[69px] md:pr-[40px] lg:pt-[200px] lg:pl-[154px] lg:pr-[141px] text-[15px]">
-          <h2 className="font-extrabold text-[32px] lg:text-[56px] mb-6 leading-tight">
+        <img src={imageStrategicTablet} className="hidden sm:block xl:hidden" />
+        <img src={imageStrategicDesktop} className="hidden xl:block" />
+        <div className="px-6 bg-black text-white py-[72px] sm:pt-[120px] md:pl-[69px] md:pr-[40px] xl:pt-[200px] xl:pl-[154px] xl:pr-[141px] text-[15px]">
+          <h2 className="font-extrabold text-[32px] xl:text-[56px] mb-6 leading-tight">
             <span className="text-red">Design </span>is
             <br className="hidden md:block" /> strategic.
           </h2>
-          <p className="mb-10 lg:text-[18px]">
+          <p className="mb-10 xl:text-[18px]">
             “A well-crafted design strategy consistently produces desired
             outcomes and brand awareness. We are firm believers that success
             lies in creative collaboration with our clients.”
           </p>
-          <button className="text-red font-extrabold text-[15px] lg:text-[18px] border-b-2 border-red p-[9px]">
+          <button className="text-red font-extrabold text-[15px] xl:text-[18px] border-b-2 border-red p-[9px]">
             Schedule a Call
           </button>
         </div>
       </div>
       <div className="bg-red md:bg-inherit px-6 py-24 md:p-0 text-white md:flex">
-        <h2 className="text-[32px] lg:text-[56px] relative z-10 leading-none lg:max-w-[705px] lg:pt-[200px] lg:pl-[165px] font-extrabold md:text-black md:pt-[126px] md:pl-[39px]">
+        <h2 className="text-[32px] xl:text-[56px] relative z-10 leading-none lg:max-w-[705px] lg:pt-[200px] lg:pl-[165px] font-extrabold md:text-black md:pt-[126px] md:pl-[39px]">
           Our approach for creating a winning brand
         </h2>
-        <ul className="mt-[49px] md:-mt-[120px] lg:flex-1 lg:-ml-[160px] lg:pr-[150px] space-y-10 md:bg-red relative md:py-[120px] md:pl-[43px] md:pr-[40px] lg:pl-[280px] lg:py-[200px]">
+        <ul className="mt-[49px] md:-mt-[120px] xl:flex-1 lg:-ml-[160px] lg:pr-[150px] space-y-10 md:bg-red relative md:py-[120px] md:pl-[43px] md:pr-[40px] lg:pl-[280px] lg:py-[200px]">
           <BulletItem order="01" title="Brand Strategy">
             Brand strategy is critical for long-term success. Outshining
             competitors and capturing the target audience are key.
@@ -90,10 +90,13 @@ function App() {
       </div>
       <div className="sm:flex sm:flex-row-reverse sm:items-start">
         <img src={imageSlide} className="sm:hidden w-full" />
-        <img src={imageSlideTablet} className="hidden sm:block lg:hidden" />
-        <img src={imageSlideDesktop} className="hidden lg:block" />
-        <div className="bg-black py-16 px-6 sm:-mr-10 lg:-mr-[190px] relative sm:py-[100px] sm:pl-[39px] sm:pr-[70px] lg:pt-[152px] lg:pl-[165px] lg:pr-[125px]">
-          <h2 className="text-white font-extrabold text-[32px] lg:text-[56px] mb-6 leading-none">
+        <img
+          src={imageSlideTablet}
+          className="hidden sm:block xl:hidden flex-1"
+        />
+        <img src={imageSlideDesktop} className="hidden xl:block flex-1" />
+        <div className="bg-black py-16 px-6 sm:-mr-10 xl:-mr-[190px] relative sm:py-[100px] sm:pl-[39px] sm:pr-[70px] lg:pt-[152px] lg:pl-[165px] lg:pr-[125px]">
+          <h2 className="text-white font-extrabold text-[32px] xl:text-[56px] mb-6 leading-none">
             Brand naming & guidelines
           </h2>
           <div className="space-x-4">
@@ -106,8 +109,8 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="py-[100px] px-6 md:flex md:items-center md:space-x-24 justify-between lg:px-[165px]">
-        <h2 className="text-[32px] lg:text-[56px] font-extrabold mb-[47px]">
+      <div className="py-[100px] px-6 md:flex md:items-center md:space-x-24 justify-between sm:px-10 xl:px-[165px]">
+        <h2 className="text-[32px] xl:text-[56px] font-extrabold mb-[47px] sm:mb-0">
           Let's build something
           <br /> great together.
         </h2>
